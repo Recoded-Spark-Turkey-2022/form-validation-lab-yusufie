a82e364a3ee93648cfdfd4110fda43ca39dd5aef
yusuf <yusufpirejman@gmail.com>
2022-11-30 00:07:01 +0300

    Completed Assignment

---
+++ src/Form.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import "./App.css";
+
+
+function Form() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+
+  return (
+    <form>
+      <input
+        value={firstName}
+        onChange={e => setFirstName(e.target.value)}
+        placeholder="First name"
+        type="text"
+        name="firstName"
+        required
+      />
+      <input
+        value={lastName}
+        onChange={e => setLastName(e.target.value)}
+        placeholder="Last name"
+        type="text"
+        name="lastName"
+        required
+      />
+        <input
+        value={phoneNumber}
+        onChange={e => setPhoneNumber(e.target.value)}
+        placeholder="Phone Number"
+        type="number"
+        name="PhoneNumber"
+        required
+      />
+      <input
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+        placeholder="Email address"
+        type="email"
+        name="email"
+        required
+      />
+      <input
+        value={password}
+        onChange={e => setPassword(e.target.value)}
+        placeholder="Password"
+        type="password"
+        name="password"
+        pattern="[a-z0-5]{8,}"
+        required
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+export default Form;
